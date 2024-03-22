8f5fa3c816d22f45018e89d571c94f79f35f04c6
dadc1007 <dadica1007@gmail.com>
2024-03-21 22:34:36 -0500

    Implemente las pruebas faltantes de las demas clases y cree un archivo para ignorar unas carpetas creadas por vscode

--- LABORATORIOS/Lab03/garden/domain/GardenTest.java
+++ LABORATORIOS/Lab03/garden/domain/GardenTest.java
@@ -1,5 +1,7 @@
 package domain;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.*;
 
 /**
@@ -17,6 +19,99 @@ public class GardenTest{
     @Before
     public void before(){
         garden = new Garden();
+    }
+
+    @Test
+    public void ShouldCreateFlower(){
+        Thing rose = garden.getThing(10, 10);
+        Thing violet = garden.getThing(15, 15);
+        Assert.assertTrue(rose instanceof Flower);
+        Assert.assertTrue(violet instanceof Flower);
+    }
+
+    @Test
+    public void shouldCreateCarnivorou(){
+        Thing venus = garden.getThing(18, 2);
+        Thing sundeuos = garden.getThing(14, 14);
+        Assert.assertTrue(venus instanceof Carnivorou);
+        Assert.assertTrue(sundeuos instanceof Carnivorou);
+    }
+
+    @Test
+    public void carnivorouShouldEatFlower(){
+        garden.setThing(31, 5, null);
+        garden.setThing(35, 12, null);
+        garden.setThing(20, 11, null);
+        garden.setThing(17, 30, null);
+        garden.setThing(20, 33, null);
+        garden.setThing(7, 5, null);
+
+        for (int i = 1; i < 10; i++) {
+            garden.ticTac();
+            if (i == 1) {
+                Thing venus = garden.getThing(15, 15);
+                Assert.assertNull(garden.getThing(14, 14));
+                Assert.assertTrue(venus instanceof Carnivorou);
+            } else if (i == 6) {
+                Thing venus = garden.getThing(10, 10);
+                Assert.assertNull(garden.getThing(11, 11));
+                Assert.assertTrue(venus instanceof Carnivorou);
+            }
+        }
+    }
+
+    @Test
+    public void shouldCreateSand(){
+        Thing thing = garden.getThing(1, 1);
+        Assert.assertTrue(thing instanceof Sand);
+
+        for (int r = 0; r < 2; r++) {
+            for (int c = 1; c < 3; c++) {
+                thing = garden.getThing(r, c);
+                Assert.assertTrue(thing instanceof Sand);
+            }
+        }
+        
+        for (int r = 0; r < 3; r++) {
+            for (int c = 35; c < Garden.LENGTH; c++) {
+                thing = garden.getThing(r, c);
+                Assert.assertTrue(thing instanceof Sand);
+            }
+        }
+    }
+
+    @Test
+    public void ShouldDisappearSand(){
+        for (int i = 1; i <= 101; i++) {
+            garden.ticTac();
+
+            if (i == 101) {
+                Thing thing = garden.getThing(1, 1);
+                Assert.assertNull(thing);
+    
+                for (int r = 0; r < 2; r++) {
+                    for (int c = 1; c < 3; c++) {
+                        thing = garden.getThing(r, c);
+                        Assert.assertNull(thing);
+                    }
+                }
+                
+                for (int r = 0; r < 3; r++) {
+                    for (int c = 35; c < Garden.LENGTH; c++) {
+                        thing = garden.getThing(r, c);
+                        Assert.assertNull(thing);
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    public void shouldCreateAromatic(){
+        Thing tulipan = garden.getThing(20, 11);
+        Thing azela = garden.getThing(17, 30);
+        Assert.assertTrue(tulipan instanceof Aromatic);
+        Assert.assertTrue(azela instanceof Aromatic);
     }
 
     @Test
@@ -97,6 +192,14 @@ public class GardenTest{
                 Assert.assertTrue(thing.isAlive());
             }
         }
+    }
+
+    @Test
+    public void shouldCreateVirus(){
+        Thing covid = garden.getThing(20, 33);
+        Thing sarampion = garden.getThing(7, 5);
+        Assert.assertTrue(covid instanceof Virus);
+        Assert.assertTrue(sarampion instanceof Virus);
     }
     
     @Test
