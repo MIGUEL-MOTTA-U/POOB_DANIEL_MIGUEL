4b43609c6c1d9c2e3087993ee822990f8366d6de
MIGUEL-MOTTA-U <miguelangelmu2016@gmail.com>
2024-04-10 21:58:21 -0500

    Añadiendo apariencia a spot Suicide

--- PROYECTO/CICLO 4/code/Suicide.java
+++ PROYECTO/CICLO 4/code/Suicide.java
@@ -8,6 +8,10 @@ import java.util.HashMap;
  * @version (CICLO 4 / 2024 - 01)
  */
 public class Suicide extends Spot {
+    private Circle leftArm;
+    private Circle rightArm;
+    private Circle downArm;
+    private Circle upArm;
     /**
      * Constructor for objects of class Suicide
      */
@@ -23,7 +27,22 @@ public class Suicide extends Spot {
     @Override
     public void addSpot(HashMap<Integer, Strand> strands) {
         super.addSpot(strands);
+        createBody();
         draw();
+    }
+    
+     /*
+     * Crete the new body of the suicide spot to differentiate it
+     */
+    private void createBody() {
+        
+        double x = this.body.getX();
+        double y = this.body.getY();
+
+        this.leftArm = new Circle(x-5, y, this.color, this.isVisible, 5);
+        this.rightArm = new Circle(x+5, y, this.color, this.isVisible, 5);
+        this.downArm = new Circle(x, y-5, this.color, this.isVisible, 5);
+        this.upArm = new Circle(x, y+5, this.color, this.isVisible, 5);
     }
 
     /**
@@ -32,5 +51,33 @@ public class Suicide extends Spot {
     @Override
     public void act() {
         this.web.delSpot(this.color);
+    }
+    
+        /*
+     * Erase the spot on screen.
+     */
+    @Override
+    protected void draw() {
+        super.draw();
+        if (isVisible) {
+            this.leftArm.makeVisible();
+            this.rightArm.makeVisible();
+            this.upArm.makeVisible();
+            this.downArm.makeVisible();
+        }
+    }
+    
+    /*
+     * Draw the spot with current specifications on screen.
+     */
+    @Override
+    protected void erase() {
+        super.erase();
+        if (isVisible) {
+            this.leftArm.makeInvisible();
+            this.rightArm.makeInvisible();
+            this.upArm.makeInvisible();
+            this.downArm.makeInvisible();
+        }
     }
 }
