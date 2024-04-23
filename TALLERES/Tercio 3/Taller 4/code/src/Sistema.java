8e89d4e438232b14b10f236cbb4b5e48277517ee
MIGUEL-MOTTA-U <miguelangelmu2016@gmail.com>
2024-04-23 11:09:57 -0500

    Trabajo hecho en clase de ultimo Taller

---
+++ TALLERES/Tercio 3/Taller 4/code/src/Sistema.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Sistema {
+    private ArrayList<Troncal> troncales;
+    private HashMap<String, Estacion> estaciones; 
+    
+}
