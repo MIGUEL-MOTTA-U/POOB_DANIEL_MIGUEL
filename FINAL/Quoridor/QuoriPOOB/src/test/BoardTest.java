36d5f34da17fb580713c15091161502c25bed737
MIGUEL-MOTTA-U <miguelangelmu2016@gmail.com>
2024-05-04 13:41:25 -0500

    Agregando espacio de trabajo para las pruebas JUnit

---
+++ FINAL/Quoridor/QuoriPOOB/src/test/BoardTest.java
@@ -0,0 +1,23 @@
+package test;
+import domain.*;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BoardTest {
+    public BoardTest() {
+    }
+    @Before
+    public void setUp() {
+    }
+    @After
+    public void tearDown() {
+    }
+    @Test
+    public void shouldCreateSquareWithRandomTokensAndHollows(){
+        
+        assertEquals(true, false);
+    }
+}
---
+++ FINAL/Quoridor/QuoriPOOB/src/test/QuoriPOOBTest.java
@@ -0,0 +1,23 @@
+package test;
+
+import domain.*;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+public class QuoriPOOBTest {
+    public QuoriPOOBTest() {
+    }
+    @Before
+    public void setUp() {
+    }
+    @After
+    public void tearDown() {
+    }
+    @Test
+    public void shouldCreateSquareWithRandomTokensAndHollows(){
+        
+        assertEquals(true, false);
+    }
+}
