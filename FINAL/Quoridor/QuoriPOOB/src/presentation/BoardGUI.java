69dad5c2b99aa72e720a88cf1ff3f91387ac2c04
dadc1007 <dadica1007@gmail.com>
2024-05-13 14:19:56 -0500

    Implemente la vista de los muros en la vista

--- FINAL/Quoridor/QuoriPOOB/src/presentation/BoardGUI.java
+++ FINAL/Quoridor/QuoriPOOB/src/presentation/BoardGUI.java
@@ -258,13 +258,35 @@ public class BoardGUI extends JPanel{
                 }
     
                 if (square.getWallUp() != null) {
-                    squareGUI.setWallUp();
+                    if (!squareGUI.getWallUp()) {
+                        squareGUI.setWallUp();
+                        squareGUI.setWallUp(true);
+                    }
                 } else if (square.getWallLeft() != null) {
-                    squareGUI.setWallLeft();
+                    if (!squareGUI.getWallLeft()) {
+                        squareGUI.setWallLeft();
+                        squareGUI.setWallLeft(true);
+                    }
                 } else if (square.getWallDown() != null) {
-                    squareGUI.setWallDown();
+                    if (!squareGUI.getWallDown()) {
+                        squareGUI.setWallDown();
+                        squareGUI.setWallDown(true);
+                    }
                 } else if (square.getWallRight() != null) {
-                    squareGUI.setWallRight();
+                    if (!squareGUI.getWallRight()) {
+                        squareGUI.setWallRight();
+                        squareGUI.setWallRight(true);
+                    }
+                } else {
+                    if (squareGUI.getWallUp()) {
+                        squareGUI.delWallUp();
+                    } else if (squareGUI.getWallLeft()) {
+                        squareGUI.delWallLeft();
+                    }  else if (squareGUI.getWallDown()) {
+                        squareGUI.delWallDown();
+                    }  else if (squareGUI.getWallRight()) {
+                        squareGUI.delWallRight();
+                    }
                 }
             }
         }
