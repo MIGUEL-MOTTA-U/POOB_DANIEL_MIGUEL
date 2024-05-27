1d2b65aafb22b0af0b405fe8057e21fb515d51bc
dadc1007 <dadica1007@gmail.com>
2024-05-27 13:35:13 -0500

    Documente la clase TurnTimerTask

--- FINAL/Quoridor/QuoriPOOB/src/domain/TurnTimerTask.java
+++ FINAL/Quoridor/QuoriPOOB/src/domain/TurnTimerTask.java
@@ -4,19 +4,40 @@ import java.io.Serializable;
 import java.util.TimerTask;
 import javax.swing.JOptionPane;
 
+/**
+ * this class represents the actions to be executed during a player's turn
+ * 
+ * @author Daniel Diaz and Miguel Motta
+ * @version 1.0
+ * @since 25-05-2024
+ */
 public class TurnTimerTask extends TimerTask implements Serializable {
     private Mode mode;
     private int remainingTime;
-    
+
+    /**
+     * Constructor of TurnTimerTask
+     * 
+     * @param mode      the game mode
+     * @param timeLimit the time limit of the game
+     */
     public TurnTimerTask(Mode mode, int timeLimit) {
         this.mode = mode;
         this.remainingTime = timeLimit;
     }
 
+    /**
+     * Return the time remaining
+     * 
+     * @return the time remaining
+     */
     public int getRemainingTime() {
         return this.remainingTime;
     }
 
+    /**
+     * the action to be taken when time is running out
+     */
     @Override
     public void run() {
         try {
