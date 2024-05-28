7d869f777968afc885a5a8d6f3f5fb53b52828c5
MIGUEL-MOTTA-U <miguelangelmu2016@gmail.com>
2024-05-28 18:03:38 -0500

    Documentacion de SquareGUI

--- FINAL/Quoridor/QuoriPOOB/src/presentation/SquareGUI.java
+++ FINAL/Quoridor/QuoriPOOB/src/presentation/SquareGUI.java
@@ -6,7 +6,13 @@ import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.border.Border;
 
-
+/**
+ * Create the component of the squares in the board and the walls
+ * 
+ * @author Daniel Diaz and Miguel Motta
+ * @version 1.0
+ * @since 25-05-2024
+ */
 public class SquareGUI extends JPanel {
     private QuoridorGUI quoridorGUI;
     private BoardGUI boardGUI;
@@ -29,6 +35,14 @@ public class SquareGUI extends JPanel {
     JButton buttonWallLeft;
     JButton buttonWallRight;
 
+    /**
+     * Constructor os SquareGUI
+     * 
+     * @param quoridorGUI the main class quoridorGUI
+     * @param boardGUI the board GUI where are located the squares and walls
+     * @param row the row of the square
+     * @param column the column of the square
+     */
     public SquareGUI(QuoridorGUI quoridorGUI, BoardGUI boardGUI, int row, int column) {
         this.quoridorGUI = quoridorGUI;
         this.boardGUI = boardGUI;
@@ -47,6 +61,9 @@ public class SquareGUI extends JPanel {
         prepareElements();
     }
 
+    /*
+     * Create all the elements
+     */
     private void prepareElements() {
         setBackground(Color.WHITE);
         setLayout(new BorderLayout());
@@ -63,6 +80,9 @@ public class SquareGUI extends JPanel {
         add(buttonWallRight, BorderLayout.EAST);
     }
 
+    /*
+     * Create the list of walls
+     */
     private void showTypeWallDialog(JButton button) {
         JComboBox<String> walls = createWallComboBox();
 
@@ -82,6 +102,9 @@ public class SquareGUI extends JPanel {
         }
     }
 
+    /*
+     * Create the button of list of walls
+     */
     private JComboBox<String> createWallComboBox() {
         JComboBox<String> walls = new JComboBox<>();
         walls.addItem("Normal");
@@ -92,6 +115,9 @@ public class SquareGUI extends JPanel {
         return walls;
     }
 
+    /*
+     * Create a button
+     */
     private JButton createButton(int width, int height) {
         JButton button = new JButton();
         button.setFocusPainted(false);
@@ -105,6 +131,9 @@ public class SquareGUI extends JPanel {
         return button;
     }
 
+    /*
+     * Create a mause listener for the interaction of the walls and the user
+     */
     private void createMouseListener(JButton button) {
         button.addMouseListener(new MouseAdapter() {
             public void mouseEntered(MouseEvent ev) {
@@ -120,6 +149,9 @@ public class SquareGUI extends JPanel {
         });
     }
 
+    /*
+     * Create a Button
+     */
     private void createActionListener(JButton button) {
         button.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ev) {
@@ -128,22 +160,41 @@ public class SquareGUI extends JPanel {
         });
     }
 
+    /**
+     * Draw a token in the board
+     */
     public void drawToken() {
         this.drawToken = true;
     }
 
+    /**
+     * Erase a token of the board
+     */
     public void eraseToken() {
         this.drawToken = false;
     }
 
+    /**
+     * Asssign a color to the token
+     * 
+     * @param color the color of the token
+     */
     public void setColorToken(Color color) {
         this.colorToken = color;
     }
 
+    /**
+     * Assign a wall to the square
+     * @param bool assign a wall up of the square if true, false otherwise
+     */
     public void setWallUp(boolean bool) {
         this.wallUp = bool;
     }
 
+    /**
+     * Assign a wall to the square
+     * @param bool assign a wall up of the square if true, false otherwise
+     */
     public void setWallLeft(boolean bool) {
         this.wallLeft = bool;
     }
@@ -152,50 +203,89 @@ public class SquareGUI extends JPanel {
         this.wallDown = bool;
     }
 
+    /**
+     * Assign a wall to the square
+     * @param bool assign a wall up of the square if true, false otherwise
+     */
     public void setWallRight(boolean bool) {
         this.wallRight = bool;
     }
 
+    /**
+     * Get a boolean value if is a wall up the square
+     * @return true if is a wall up, false otherwise
+     */
     public boolean getWallUp() {
         return this.wallUp;
     }
 
+    /**
+     * Get a boolean value if is a wall left the square
+     * @return true if is a wall left, false otherwise
+     */
     public boolean getWallLeft() {
         return this.wallLeft;
     }
 
+    /**
+     * Get a boolean value if is a wall down the square
+     * @return true if is a wall down, false otherwise
+     */
     public boolean getWallDown() {
         return this.wallDown;
     }
 
+    /**
+     * Get a boolean value if is a wall right the square
+     * @return true if is a wall right, false otherwise
+     */
     public boolean getWallRight() {
         return this.wallRight;
     }
 
+    /**
+     * Assign a color to the wall up
+     * @param color assign a color to a wall up
+     */
     public void setWallUp(Color color) {
         setBorder(paintBorder(buttonWallUp, color));
         this.colorBorderUp = color;
         remove(buttonWallUp);
     }
 
+    /**
+     * Assign a color to the wall left
+     * @param color assign a color to a wall left
+     */
     public void setWallLeft(Color color) {
         setBorder(paintBorder(buttonWallLeft, color));
         this.colorBorderLeft = color;
         remove(buttonWallLeft);
     }
 
+    /**
+     * Assign a color to the wall down
+     * @param color assign a color to a wall down
+     */
     public void setWallDown(Color color) {
         setBorder(paintBorder(buttonWallDown, color));
         this.colorBorderDown = color;
         remove(buttonWallDown);
     }
 
+    /**
+     * Assign a color to the wall right
+     * @param color assign a color to a wall right
+     */
     public void setWallRight(Color color) {
         setBorder(paintBorder(buttonWallRight, color));
         this.colorBorderRight = color;
         remove(buttonWallRight);
     }
 
+    /**
+     * Remove the up wall
+     */
     public void delWallUp() {
         setBorder(paintBorder(buttonWallUp, Color.BLACK));
         this.colorBorderUp = Color.BLACK;
@@ -203,6 +293,9 @@ public class SquareGUI extends JPanel {
         add(buttonWallUp, BorderLayout.NORTH);
     }
 
+    /**
+     * Remove the left wall
+     */
     public void delWallLeft() {
         setBorder(paintBorder(buttonWallLeft, Color.BLACK));
         this.colorBorderLeft = Color.BLACK;
@@ -210,6 +303,9 @@ public class SquareGUI extends JPanel {
         add(buttonWallLeft, BorderLayout.WEST);
     }
 
+    /**
+     * Remove the down wall
+     */
     public void delWallDown() {
         setBorder(paintBorder(buttonWallDown, Color.BLACK));
         this.colorBorderDown = Color.BLACK;
@@ -217,6 +313,9 @@ public class SquareGUI extends JPanel {
         add(buttonWallDown, BorderLayout.SOUTH);
     }
 
+    /**
+     * Remove the right wall
+     */
     public void delWallRight() {
         setBorder(paintBorder(buttonWallRight, Color.BLACK));
         this.colorBorderRight = Color.BLACK;
@@ -224,6 +323,11 @@ public class SquareGUI extends JPanel {
         add(buttonWallRight, BorderLayout.EAST);
     }
 
+    /**
+     * Paint the square of a given color
+     * 
+     * @param color the color of the square background
+     */
     public void paintBackgroung(Color color) {
         setBackground(color);
         buttonWallUp.setBackground(color);
@@ -232,6 +336,9 @@ public class SquareGUI extends JPanel {
         buttonWallRight.setBackground(color);
     }
 
+    /*
+     * Add a wall to the square
+     */
     private boolean addWall(String type, JButton button) {
         String squareSide;
         boolean wallAdded = true;
@@ -262,6 +369,9 @@ public class SquareGUI extends JPanel {
         return wallAdded;
     }
 
+    /*
+     * Paint the border of a square (the wall)
+     */
     private Border paintBorder(JButton button, Color color) {
         Border borderUp = createMatteBorder(button == buttonWallUp ? color : colorBorderUp, 1, 0, 0, 0);
         Border borderLeft = createMatteBorder(button == buttonWallLeft ? color : colorBorderLeft, 0, 1, 0, 0);
@@ -273,6 +383,9 @@ public class SquareGUI extends JPanel {
         return BorderFactory.createCompoundBorder(topLeft, bottomRight);
     }
 
+    /*
+     * Create the default border of a square
+     */
     private Border defaultBorder() {
         Border borderUp = createMatteBorder(colorBorderUp, 1, 0, 0, 0);
         Border borderLeft = createMatteBorder(colorBorderLeft, 0, 1, 0, 0);
@@ -284,6 +397,9 @@ public class SquareGUI extends JPanel {
         return BorderFactory.createCompoundBorder(topLeft, bottomRight);
     }
     
+    /*
+     * Create a Border
+     */
     private Border createMatteBorder(Color color, int top, int left, int bottom, int right) {
         return BorderFactory.createMatteBorder(top, left, bottom, right, color);
     }
